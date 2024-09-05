12bd3cf47899352d2cee4b4195a494a46a5a2106
Siva Ram Reddy Kathi <kathishivaram@gmail.com>
2024-09-05 11:39:44 -0500

    Add navigation stack on each tab

--- Appetizer/AppetizerListView.swift
+++ Appetizer/AppetizerListView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct AppetizerListView: View {
     var body: some View {
+        NavigationStack {
+            Text("Appetizer List View")
+                .navigationTitle("Appetizer")
+        }
         
-        Text("Appetizer List View")
     }
 }
 
