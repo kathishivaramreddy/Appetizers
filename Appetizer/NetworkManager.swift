53e2bf97f6a75abb84de1786f90ce623ec3e2184
Siva Ram Reddy Kathi <kathishivaram@gmail.com>
2024-09-07 11:52:47 -0500

    Add network manager

---
+++ Appetizer/NetworkManager.swift
@@ -0,0 +1,60 @@
+//
+//  NetworkManager.swift
+//  Appetizer
+//
+//  Created by Shiva on 9/6/24.
+//
+
+import Foundation
+
+final class NetworkManager {
+    
+    static let shared = NetworkManager()
+    
+    private init() {}
+    
+    static let baseURL = "https://seanallen-course-backend.herokuapp.com/swiftui-fundamentals/"
+    
+    private let appetizerURL = "appetizers"
+    
+    func getAppetizers(completionHandler: @escaping (Result<[Appetizer], APError>) -> Void) {
+        
+        guard let url = URL(string: appetizerURL) else {
+            
+            completionHandler(.failure(.invalidURL))
+            return
+        }
+        
+        let urlRequest = URLRequest(url: url)
+        
+         let task = URLSession.shared.dataTask(with: urlRequest, completionHandler: { data, response, error in
+            
+            guard let _ = error else {
+                
+                completionHandler(.failure(.unableToComplete))
+                return
+            }
+             
+             guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+              
+                 completionHandler(.failure(.invalidResponse))
+                 return
+             }
+             
+             guard let data = data else {
+                 completionHandler(.failure(.invalidData))
+                 return
+             }
+             
+             let decoder = JSONDecoder()
+             
+             do {
+                 let decodedData = try decoder.decode(AppetizerResponse.self, from: data)
+                 completionHandler(.success(decodedData.request))
+             } catch {
+                 completionHandler(.failure(.invalidData))
+             }
+        })
+        task.resume()
+    }
+}
