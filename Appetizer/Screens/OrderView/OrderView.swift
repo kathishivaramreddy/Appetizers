4b7d4df08f925bb77cf33f8f1e91f02696d68253
Siva Ram Reddy Kathi <kathishivaram@gmail.com>
2024-09-16 12:53:26 -0500

    Add order view

--- Appetizer/Screens/OrderView/OrderView.swift
+++ Appetizer/Screens/OrderView/OrderView.swift
@@ -8,9 +8,40 @@
 import SwiftUI
 
 struct OrderView: View {
+    
+    @State var orderedAppetizer = MockData.orderItems
+    @State var totalOrderPrice = 0.0
+    
     var body: some View {
         NavigationStack {
-            Text("Order View")
+            VStack {
+                List {
+                    
+                    ForEach(orderedAppetizer) {  appetizer in
+                        
+                        AppetizerListCellView(appetizer: appetizer)
+                        
+                    }.onDelete(perform: { indexSet in
+                        for index in indexSet {
+                            orderedAppetizer.remove(at: index)
+                        }
+                        totalOrderPrice = Double(orderedAppetizer.reduce(0) { partialResult, appetizer in
+                            partialResult + appetizer.price
+                        })
+                    })
+                  
+                    
+                }
+                
+                Spacer()
+                APButton(title: "$\(totalOrderPrice, specifier: "%.2f") Place Order")
+                    .padding(.bottom, 20)
+                    .onAppear {
+                        totalOrderPrice = Double(orderedAppetizer.reduce(0) { partialResult, appetizer in
+                            partialResult + appetizer.price
+                        })
+                    }
+            }
                 .navigationTitle("Order")
         }
        
